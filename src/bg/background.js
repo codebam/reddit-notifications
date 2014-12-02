497385d9b324a59c94326c103566ac17ea662f59
Sean Behan <swb1998@gmail.com>
2014-12-01 22:16:57 -0500

    realized that theres no point. I'll just untrack it in git so people dont see my link

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,11 +1,25 @@
 var inbox = [];
 window.setInterval(function() {
     var reddit = $.ajax({
-    url: "ajax.json",
-    context: document.body,
-    success: function(){
-      $(this).addClass("done");
-    }
-});
+        url: // insert "unread messages" json feed from https://www.reddit.com/prefs/feeds/
+        success: function(data) {
+            for (i = 0; i < data.data.children.length; ++i) {
+                if ($.inArray(data.data.children[i].data.id, inbox) == -1) {
+                  if (data.data.children[i].kind == 't4') {
+                      var text = data.data.children[i].data.author + ' Sent:\n\n' + data.data.children[i].data.body
+                  } else if (data.data.children[i].kind == 't1') {
+                      var text = data.data.children[i].data.author + ' Replied:\n\n' + data.data.children[i].data.body
+                  }
+                    var n = new Notification('Reddit', {
+                        body: text
+                    });
+                    n.onclick = function() {
+                        window.open('https://reddit.com/message/unread', '_newtab')
+                    };
+                    inbox.push(data.data.children[i].data.id)
+                }
+            }
+        },
+    });
     console.log(inbox)
 }, 5000);
